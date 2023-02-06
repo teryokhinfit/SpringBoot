0e8ff25a4649f4ba57f53a0ea9909ad744c06bdf
teryokhinfit <terekhincw@gmail.com>
2023-02-06 13:01:05 +0400

    TestContainers

--- src/test/java/ru/netology/springboot/ApplicationTests.java
+++ src/test/java/ru/netology/springboot/ApplicationTests.java
@@ -1,13 +1,53 @@
 package ru.netology.springboot;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import ru.netology.springboot.profile.DevProfile;
+import ru.netology.springboot.profile.ProductionProfile;
 
-@SpringBootTest
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ApplicationTests {
+
+    @Autowired
+    TestRestTemplate restTemplate;
+    DevProfile devProfile = new DevProfile();
+    ProductionProfile productionProfile = new ProductionProfile();
+
+    @Container
+    public static GenericContainer<?> myapp1 = new GenericContainer<>("devapp")
+            .withExposedPorts(8080);
+
+    @Container
+    public static GenericContainer<?> myapp2 = new GenericContainer<>("prodapp")
+            .withExposedPorts(8081);
+
+    @BeforeAll
+    public static void setUp() {
+        myapp1.start();
+        myapp2.start();
+
+    }
 
     @Test
     void contextLoads() {
+        ResponseEntity<String> response1 = restTemplate.getForEntity("http://localhost:" +
+                myapp1.getMappedPort(8080) +
+                "/profile", String.class);
+        Assertions.assertEquals(response1.getBody(), devProfile.getProfile());
+
+        ResponseEntity<String> response2 = restTemplate.getForEntity("http://localhost:" +
+                myapp2.getMappedPort(8081) +
+                "/profile", String.class);
+        Assertions.assertEquals(response2.getBody(), productionProfile.getProfile());
     }
 
 }
